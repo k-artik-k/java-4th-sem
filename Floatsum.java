5d704bcda8d5ddab41e93a20efb2b7aeaabf04ee
CSE-L210-55 <CSE-210-55@gitam.edu>
2024-12-11 15:36:23 +0530

    changed for assignment

--- Floatsum.java
+++ Floatsum.java
@@ -1,21 +1,13 @@
 class Floatsum {
     public static void main(String[] args) {
-        // Ensure that two arguments are passed
-        if (args.length < 2) {
-            System.out.println("Please enter two decimal numbers.");
-            return;
-        }
+    
+        double num = Double.parseDouble(args[0]);
+        double num1 = Double.parseDouble(args[1]);
 
-        // Parse the input arguments as floating-point numbers
-        float num = Float.parseFloat(args[0]);
-        float num1 = Float.parseFloat(args[1]);
-
-        // Round the numbers to 3 decimal places
-        float roundedNum = Math.round(num * 1000) / 1000.0f;
-        float roundedNum1 = Math.round(num1 * 1000) / 1000.0f;
-
-        // Check if the rounded numbers are equal
-        if (roundedNum == roundedNum1) {
+       
+        num = Math.round(num * 1000.0) / 1000.0;
+        num1 = Math.round(num1 * 1000.0) / 1000.0;
+        if (num1 == num) {
             System.out.println("The numbers are equal up to 3 decimal places.");
         } else {
             System.out.println("The numbers are not equal up to 3 decimal places.");
