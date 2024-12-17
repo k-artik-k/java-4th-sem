7d00ffd273094ab55da3091588e26c8fa83c17d1
k-artik-k <kpappula@gitam.in>
2024-12-17 21:12:17 +0530

    prime number

---
+++ scannerclass/PrimeNumbersUpToN.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrimeNumbersUpToN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Prime numbers up to " + n + " are:");
+        
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+        scanner.close();
+    }
+}
